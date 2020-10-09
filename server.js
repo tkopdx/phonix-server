755bf43a437a5bd052d1c7f7687b3d3389321fdf
tkopdx <63491466+tkopdx@users.noreply.github.com>
2020-10-09 14:53:26 +0900

    changed port to 8000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 const app = express();
 const logger = require('morgan');
 
